bb730c109306dc037b16ab2d64a6aca29cd34e9a
Joseph Merineau <joseph.merineau@gmail.com>
2017-03-21 14:39:28 -0400

    Un_bon_depart

--- etape.js
+++ etape.js
@@ -9,5 +9,26 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
+var db // variable qui contiendra le lien sur la BD
 
+MongoClient.connect('mongodb://127.0.0.1:27017/examf2', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(8081, () => {
+    console.log('Connexion à la BD et on écoute sur le port 8081')
+  })
+})
 
+app.get('/',  (req, res) => {
+   console.log('la route route get / = ' + req.url)
+ 
+    var cursor = db.collection('examf2').find().toArray(function(err, resultat){
+       if (err) return console.log(err)
+    // renders index.ejs
+    // affiche le contenu de la BD
+    res.render('index.ejs', {examf2: resultat})
+
+    }) 
+    
+
+})
